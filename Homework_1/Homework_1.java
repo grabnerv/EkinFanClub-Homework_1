88c93146a8a30490e39f4c63e73b71d342cf203e
Van Grabner <grabner737@gmail.com>
2024-01-31 16:11:54 -0600

    create file, add some names

---
+++ Homework_1/Homework_1.java
@@ -0,0 +1,9 @@
+public class Homework_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Van Grabner - grabnerv");
+		System.out.println("Temitope Alabi - Keylith2020");
+		System.out.println("Mel Nezirovic - amelll1");
+	}
+
+}
