aad1223d919023970fe301729fb123927cd83ae6
Van Grabner <grabner737@gmail.com>
2024-02-01 13:17:22 -0600

    finish adding group members to list

--- Homework_1/Homework_1.java
+++ Homework_1/Homework_1.java
@@ -4,6 +4,7 @@ public class Homework_1 {
 		System.out.println("Van Grabner - grabnerv");
 		System.out.println("Temitope Alabi - Keylith2020");
 		System.out.println("Mel Nezirovic - amelll1");
+		System.out.println("Micheal Whaley - micheal-whaley");
 	}
 
 }
