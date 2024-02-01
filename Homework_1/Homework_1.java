19c2ff986fc08da3e2c7030495e907804c475cd6
Van Grabner <grabner737@gmail.com>
2024-02-01 13:21:36 -0600

    refactor class

--- Homework_1/Homework_1.java
+++ Homework_1/Homework_1.java
@@ -1,10 +1,8 @@
 public class Homework_1 {
-
 	public static void main(String[] args) {
 		System.out.println("Van Grabner - grabnerv");
 		System.out.println("Temitope Alabi - Keylith2020");
 		System.out.println("Mel Nezirovic - amelll1");
 		System.out.println("Micheal Whaley - micheal-whaley");
 	}
-
 }
