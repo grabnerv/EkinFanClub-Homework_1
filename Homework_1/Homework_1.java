4efe14c92b361ab33029b6d5e9aeb852f6cab9cd
Micheal Whaley <michealwhaley42@gmail.com>
2024-02-01 19:15:14 -0600

    added github ids

--- Homework_1/Homework_1.java
+++ Homework_1/Homework_1.java
@@ -1,8 +1,8 @@
 public class Homework_1 {
 	public static void main(String[] args) {
-		System.out.println("Van Grabner - grabnerv");
-		System.out.println("Temitope Alabi - Keylith2020");
-		System.out.println("Mel Nezirovic - amelll1");
+		System.out.println("Van Grabner - 56400638");
+		System.out.println("Temitope Alabi - 72046918");
+		System.out.println("Mel Nezirovic - 72543665");
 		System.out.println("Micheal Whaley - 44105295");
 	}
 }
